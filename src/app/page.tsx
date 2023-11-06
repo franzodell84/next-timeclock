410d482d3433502ff5ae653d45e0513f19207453
Franz Caangay <franz@rschooltoday.com>
2023-11-06 17:42:26 +0800

    Clean-up files
    - Clean up unnecessary files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
-import UserTimeIn from "/.userlogs";
+
 export default function Home() {
   return (
     <main>
       <div className="grid grid-cols-4 gap-4">
         <div>
           <h3>Please sign in below</h3>
-          <UserTimeIn />
         </div>
         <div>
           <h3>Logs for today</h3>
